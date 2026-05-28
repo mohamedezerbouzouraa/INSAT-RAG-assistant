79284a657b140f0f0f383db87fd48081d3a29091
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-05-28 11:38:27 +0100

    Add BM25Retriever initialization in retriever.py

---
+++ backend/rag/retriever.py
@@ -0,0 +1,8 @@
+from langchain_community.retrievers import BM25Retriever
+from rag.documents import documents
+from config import TOP_K
+
+retriever = BM25Retriever.from_documents(
+    documents=documents,
+    k=TOP_K
+)
